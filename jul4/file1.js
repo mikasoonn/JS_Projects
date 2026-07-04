fc87eb136172bb93cce7151fa9f386853e9a1011
Sona Mikayelyan <sonamikayelyan2006.com>
2026-07-04 20:38:19 +0400

    adding jul4

---
+++ jul4/file1.js
@@ -0,0 +1,33 @@
+//1 
+// const items = [100, 200, 300];
+// var first, second;
+// [first, second] = [...items];
+// console.log(first);
+// console.log(second);
+
+//2
+// const data = ['apple', 'banana', 'orange', 'pear'];
+// var fruit1, fruit3;
+// [fruit1, , fruit3] = [...data];
+// console.log(fruit1);
+// console.log(fruit3);
+
+//3
+// const numbers = [1, 2, 3, 4, 5];
+// var a, b, rest;
+// [a, b, ...rest] = [...numbers];
+// console.log(a);
+// console.log(b);
+// console.log(rest);
+
+//4
+// let x = 10;
+// let y = 20;
+// [y, x] = [x, y];
+// console.log(x);
+// console.log(y);
+
+//5
+// let arr = [10, 20, 30, 40];
+// [arr[1], arr[0], arr[3], arr[2]] = [...arr];
+// console.log(arr);
